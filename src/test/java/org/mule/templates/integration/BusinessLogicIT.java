3452ded32c0806414b825ae3a6c755543ddc0650
Marek <marecica2@gmail.com>
2014-07-03 11:59:39 +0200

    SIT-430

--- src/test/java/org/mule/templates/integration/BusinessLogicIT.java
+++ src/test/java/org/mule/templates/integration/BusinessLogicIT.java
@@ -95,7 +95,6 @@ public class BusinessLogicIT extends FunctionalTestCase {
 		// Flow to retrieve accounts from target system after sync in g
 		retrieveAccountFromSalesforceFlow = getSubFlow("retrieveAccountFromSalesforceFlow");
 		retrieveAccountFromSalesforceFlow.initialise();
-	
 		createTestDataInSandBox();
 	}
 
